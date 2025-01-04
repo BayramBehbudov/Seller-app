a9865716f91181783097328b7ff1b2d8a9c0f059
BayramBehbudov <bayram.behbudov@gmail.com>
2025-01-04 20:44:26 +0400

    update

--- context/NotifyProvider.tsx
+++ context/NotifyProvider.tsx
@@ -14,7 +14,7 @@ import { IUserDB } from "@/types/interfaces";
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: true,
+    shouldPlaySound: false,
     shouldSetBadge: true,
   }),
 });
@@ -23,57 +23,6 @@ interface INotifyContext {}
 const NotifyContext = createContext<INotifyContext>({});
 
 export const useNotifyContext = () => useContext(NotifyContext);
-
-async function registerForPushNotificationsAsync() {
-  let token: string | undefined = undefined;
-
-  if (Platform.OS === "android") {
-    await Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-
-    let finalStatus = existingStatus;
-
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-
-    if (finalStatus !== "granted") return;
-
-    token = (
-      await Notifications.getExpoPushTokenAsync({
-        projectId: "80c6c315-cfc6-4a45-88d0-4d80ae68654b",
-      })
-    ).data;
-  } else {
-    Alert.alert("Xeta", "Bu funksiya yalnız cihazlarda işləyir");
-  }
-
-  return token;
-}
-
-const sendTokenToServer = async (userId: string, token: string) => {
-  try {
-    const response = await axios.post(
-      `https://express-bay-rho.vercel.app/api/notification/save-token`,
-      { userId, token }
-    );
-    if (response.status === 200) {
-      console.log("Token sent to server successfully");
-    }
-  } catch (error) {
-    console.error("Error sending token to server:", error);
-  }
-};
 
 export const NotifyProvider = ({
   children,
@@ -86,23 +35,56 @@ export const NotifyProvider = ({
 }) => {
   const responseListener = useRef<any>();
 
-  useEffect(() => {
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((res: any) => {
-        const data = res.notification.request.content.data;
-        console.log("Notify data", data);
+  async function registerForPushNotificationsAsync() {
+    let token: string | undefined = undefined;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
       });
-
-    return () => {
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (user?._id) {
-      setupNotifications();
     }
-  }, [user?._id]);
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") return;
+
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: "80c6c315-cfc6-4a45-88d0-4d80ae68654b",
+        })
+      ).data;
+    } else {
+      Alert.alert("Xeta", "Bu funksiya yalnız cihazlarda işləyir");
+    }
+
+    return token;
+  }
+
+  const sendTokenToServer = async (userId: string, token: string) => {
+    try {
+      const response = await axios.post(
+        `https://express-bay-rho.vercel.app/api/notification/save-token`,
+        { userId, token }
+      );
+      if (response.status === 200) {
+        console.log("Token sent to server successfully");
+      }
+    } catch (error) {
+      console.error("Error sending token to server:", error);
+    }
+  };
 
   const setupNotifications = async () => {
     const token = await registerForPushNotificationsAsync();
@@ -114,6 +96,32 @@ export const NotifyProvider = ({
       pushTokens: [...(prevUser?.pushTokens || []), token],
     }));
   };
+
+  useEffect(() => {
+    if (user?._id) {
+      setupNotifications();
+    }
+
+    // bildiriş alınan kimi işə düşür
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        const content = notification.request.content;
+        console.log("Bildiriş alındı:", content);
+      }
+    );
+
+    // bildirişə klik olanda işə düşür
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((res: any) => {
+        const content = res.notification.request.content;
+        console.log("Notify data", content);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(responseListener.current);
+      Notifications.removeNotificationSubscription(notificationListener);
+    };
+  }, [user?._id]);
 
   return <NotifyContext.Provider value={{}}>{children}</NotifyContext.Provider>;
 };
--- context/OrdersProvider.tsx
+++ context/OrdersProvider.tsx
@@ -6,7 +6,6 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import { useGlobalContext } from "./GlobalProvider";
 import axios from "axios";
 
 interface IOrdersContext {
